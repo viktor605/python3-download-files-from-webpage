2319a06fb36001759619599745786ae5da4c5a46
viktor605 <31222382+viktor605@users.noreply.github.com>
2018-04-10 16:22:37 +0300

    Create urls-download.py

---
+++ urls-download.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from bs4 import BeautifulSoup
+import urllib.request
+from urllib.request import Request, urlopen, urlretrieve
+
+#url = "https://cs.brown.edu/~rt/gdhandbook/"
+url = input()
+resp = urllib.request.urlopen(url)
+soup = BeautifulSoup(resp, "lxml", from_encoding=resp.info().get_param('charset'))
+links = []
+for link in soup.find_all('a', href=True):
+    links.append(link.get('href'))
+    
+for elem in links:
+    elem = '%s%s' % (url, elem)
+    print(elem)
+    destination = url.rsplit('/',1)[1]
+    urlretrieve(elem, destination)
